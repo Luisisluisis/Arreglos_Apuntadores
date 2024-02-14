d007f65d692c8f62750df65dea1dfd74710b648d
Luisisluisis <158091978+Luisisluisis@users.noreply.github.com>
2024-02-14 14:47:08 -0500

    ejercicio 1 y 2 de la clase

---
+++ Matrices/dulces.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+struct product {
+    double price;
+    int posicion;
+};
+
+int main() {
+    int amountProducts;
+    cout<<"Digit the amount of products you want on the machine";
+    cin >> amountProducts;
+
+    const int columnas= 3;
+
+    product machine[amountProducts][columnas];
+
+    for (int i=0; i <)
+    
+    machine[0][0] = {"Coca-cola", 1.000, 1}
+
+    for (int i=0; i<5; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << calculator[i][j] << "";
+        }
+    }
+    return 0;
+}
